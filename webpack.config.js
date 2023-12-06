ff03e54d911f2fbf984f8c2558c6f83cdab94fc8
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-06 13:31:57 -0500

    create starting files, set up webpack, set up dev environment

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlPlugIn = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugIn({
+      title: 'TaskBrew',
+      filename: 'index.html',
+      inject: 'head',
+      template: './src/template.html',
+    }),
+  ],
+};
